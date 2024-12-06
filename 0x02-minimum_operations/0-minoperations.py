7067a3013905f7a5810b7215c6d6fbb2ca109e2f
Ahmad <ahmad.new.m.v@gmail.com>
2024-12-06 07:41:39 +0000

    The solution

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Minimum Operations Problem
+"""
+
+
+primes = [2]
+
+def is_prime(number):
+    """Determining if a number is prime
+    """
+    if not number % 2 and number != 2:
+        return False
+    for i in range(3, number // 2, 2):
+        if not number % i:
+            return False
+    return True
+
+def minOperations(number):
+    """Main function
+    """
+    if number <= 0:
+        return 0
+    if is_prime(number):
+        return number
+    for i in range(3, number // 2, 2):
+        if is_prime(i):
+            primes.append(i)
+    total = 0
+    i = 0
+    while i < len(primes) and number > 0:
+        if not number % primes[i]:
+            total += primes[i]
+            number /= primes[i]
+            i = 0
+            continue
+        i += 1
+    return total
