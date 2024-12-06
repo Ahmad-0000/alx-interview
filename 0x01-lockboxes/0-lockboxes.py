761d4627ecdfc32adf4eb636872d3f26c20786c0
Ahmad <ahmad.new.m.v@gmail.com>
2024-12-06 05:43:36 +0000

    Improved solution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,18 +7,23 @@ LockBoxes problem
 def canUnlockAll(boxes):
     """Checks if "boxes" can be unlocked
     """
-    open_boxes_set = set(boxes[0])
-    temp_set = open_boxes_set.copy()
-    for i in range(len(boxes)):
-        for key in temp_set:
-            try:
-                for j in boxes[key]:
-                    open_boxes_set.add(j)
-            except IndexError:
-                pass
-        for k in open_boxes_set:
-            temp_set.add(k)
+    open_boxes_keys = set(boxes[0])
+    temp_set = set(boxes[0])
+    for box_idx, box in enumerate(boxes):
+        if box_idx not in temp_set:
+            pass
+        else:
+            for new_key in box:
+                open_boxes_keys.add(new_key)
+                if new_key < box_idx and new_key not in temp_set:
+                    try:
+                        previous_box = boxes[new_key].copy()
+                        for k in previous_box:
+                            open_boxes_keys.add(k)
+                    except IndexError:
+                        pass
+        temp_set = open_boxes_keys.copy()
     for i in range(1, len(boxes)):
-        if i not in open_boxes_set:
+        if i not in open_boxes_keys:
             return False
     return True
