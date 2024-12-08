90591bfcbfe9a2b31265354531dc07193784fb16
Ahmad <ahmad.new.m.v@gmail.com>
2024-12-08 04:48:14 +0200

    ...

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,12 +3,12 @@
 UTF-8 Validation Problem
 """
 
+
 def validUTF8(data):
     """Validates if data is a valid utf8-encoded data
     """
     for chunck in data:
-        if chunck > 128:
-            return False
-        elif chunck < 0:
+        chunck = chunck >> 7
+        if chunck & 1:
             return False
     return True
