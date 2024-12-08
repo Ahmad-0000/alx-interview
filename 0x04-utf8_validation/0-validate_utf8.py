1d98653e6fb0aaf3498c1a711a91742a8a376ea3
Ahmad <ahmad.new.m.v@gmail.com>
2024-12-08 06:43:05 +0200

    Removed a bug

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,9 +6,34 @@ UTF-8 Validation Problem
 
 def validUTF8(data):
     """Validates if data is a valid utf8-encoded data
+
+    'data' is a list of integers, each of which represents
+    a byte in the data sequence, with account for only the 8
+    least significant bits
     """
-    for chunck in data:
-        chunck = chunck >> 7
-        if chunck & 1:
+    mask = 128
+    temp = 0
+    following = 0
+    i = 0
+    while i < len(data):
+        temp = data[i]
+        while temp & mask:
+            following += 1
+            temp = temp << 1
+        if following > 4:
             return False
+        try:
+            while following - 1 > 0:
+                i += 1
+                following -= 1
+                temp = data[i]
+                j = 0
+                while temp & mask:
+                    temp = temp << 1
+                    j += 1
+                if j != 1:
+                    return False
+        except IndexError:
+            return False
+        i += 1
     return True
