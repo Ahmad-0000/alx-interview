8785c63e4d1179fd6585e6edba27c143306ac3b0
Ahmad <ahmad.new.m.v@gmial.com>
2024-12-22 15:52:26 +0200

    Change variable name, 'target' to 'total'

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,21 +4,21 @@ Coins problem
 """
 
 
-def makeChange(coins, target):
+def makeChange(coins, total):
     """The solution"""
     if not coins:
         return -1
-    if target <= 0:
+    if total <= 0:
         return 0
     coins.sort()
     answer = []
-    while target > 0:
+    while total > 0:
         i = 0
-        while i != len(coins) and target >= coins[i]:
+        while i != len(coins) and total >= coins[i]:
             i += 1
         i -= 1
-        target -= coins[i]
+        total -= coins[i]
         answer.append(coins[i])
-    if target == 0:
+    if total == 0:
         return len(answer)
     return -1
